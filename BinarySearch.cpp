6aa40cf1b6b168e701c1a883ae964bcab9bcb0b3
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-04-16 11:20:43 +0700

    masukan banyaknya array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,3 +6,18 @@ int nPanjang;
 int x;
 
 void input()
+{
+    while (true)
+    {
+        cout << "masukan banyaknya pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <=10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "n[!] jumlah elemen tidak boleh lebih dari 10. silakan coba lagi. \n";
+        }
+    }
