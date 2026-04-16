543bfd8cceed508bdffee170402138cd12a4187c
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-04-16 11:23:35 +0700

    int pass

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -34,3 +34,19 @@ void input()
 }
 
 void bubblesortarray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
